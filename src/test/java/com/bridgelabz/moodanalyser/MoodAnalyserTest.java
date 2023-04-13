cc21e840fcee8908f190408221d371d9f6944c2d
kiran68 <kiranurshil88@gmail.com>
2023-04-13 18:06:05 +0530

    Add:Ability To Analyse Mood

---
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.moodanalyser;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+
+import com.sun.tools.javac.util.Assert;
+
+public class MoodAnalyserTest {
+
+	 @Test
+	    public void testAnalysisMoodSad() {
+	        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
+	        String mood = moodAnalyser.analysisMood();
+	        assertEquals("SAD", mood);
+	    }
+
+	    // Test case to check for "Happy" mood
+	    @Test
+	    public void testAnalysisMoodHappy() {
+	        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Happy Mood");
+	        String mood = moodAnalyser.analysisMood();
+	        assertEquals("HAPPY", mood);
+	    }  
+	 
+	
+}
