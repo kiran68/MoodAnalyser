8b8190a602172b1001e3f0d1f9f02024217d4ffd
kiran68 <kiranurshil88@gmail.com>
2023-04-13 18:39:03 +0530

    Add: Check the Exception With User Mood

---
+++ src/main/java/com/bridgelabz/moodanalyser/InvalidMoodException.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.moodanalyser;
+
+public class InvalidMoodException extends Exception {
+	public InvalidMoodException(String message) {
+		super(message);
+	}
+}
--- src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -1,24 +1,39 @@
 package com.bridgelabz.moodanalyser;
 
-public class MoodAnalyser {
-    private String message; // Field to store the mood message
 
-    // Default constructor
-    public MoodAnalyser() {
-        message = ""; // Set default empty message
-    }
 
-    // Constructor with message parameter
-    public MoodAnalyser(String message) {
-        this.message = message; // Set the message field with the passed parameter
-    }
+	public class MoodAnalyser {
+	    private String message; // Field to store the mood message
 
-    // Analysis mood method using the message field
-    public String analysisMood() {
-        if (message.contains("Sad")) {
-            return "SAD";
-        } else {
-            return "HAPPY";
-        }
-    }
-}
+	    // Default constructor
+	    public MoodAnalyser() {
+	        message = ""; // Set default empty message
+	    }
+
+	    // Constructor with message parameter
+	    public MoodAnalyser(String message) {
+	        this.message = message; // Set the message field with the passed parameter
+	    }
+
+	    // Analysis mood method using the message field
+	    public String analysisMood() {
+	        try {
+	            if (message == null || message.isEmpty()) {
+	                throw new InvalidMoodException("Invalid mood: Mood message cannot be null or empty.");
+	            } else if (message.contains("Sad")) {
+	                return "SAD";
+	            } else {
+	                return "HAPPY";
+	            }
+	        } catch (InvalidMoodException e) {
+	            // Handle the exception and return "HAPPY" as default value
+	            System.out.println(e.getMessage());
+	            return "HAPPY";
+	        }
+	    }
+	}
+
+
+	
+
+
