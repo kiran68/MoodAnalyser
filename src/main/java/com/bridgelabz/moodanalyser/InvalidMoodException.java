97cf8175a3da70a866e746431ca5605fb0949394
kiran68 <kiranurshil88@gmail.com>
2023-04-15 12:25:35 +0530

    Add: To check Invalid Mood

--- src/main/java/com/bridgelabz/moodanalyser/InvalidMoodException.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz.moodanalyser;
-
-public class InvalidMoodException extends Exception {
-	public InvalidMoodException(String message) {
-		super(message);
-	}
-}
--- src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -1,39 +1,55 @@
 package com.bridgelabz.moodanalyser;
 
+import java.util.HashMap;
+import java.util.Map;
 
+enum MoodEnum {
+    HAPPY("happy"),
+    SAD("sad"),
+    ANGRY("angry"),
+    NEUTRAL("neutral");
 
-	public class MoodAnalyser {
-	    private String message; // Field to store the mood message
+    private String value;
+    private static Map<String, MoodEnum> map = new HashMap<>();
 
-	    // Default constructor
-	    public MoodAnalyser() {
-	        message = ""; // Set default empty message
-	    }
+    static {
+        for (MoodEnum mood : MoodEnum.values()) {
+            map.put(mood.value, mood);
+        }
+    }
 
-	    // Constructor with message parameter
-	    public MoodAnalyser(String message) {
-	        this.message = message; // Set the message field with the passed parameter
-	    }
+    MoodEnum(String value) {
+        this.value = value;
+    }
 
-	    // Analysis mood method using the message field
-	    public String analysisMood() {
-	        try {
-	            if (message == null || message.isEmpty()) {
-	                throw new InvalidMoodException("Invalid mood: Mood message cannot be null or empty.");
-	            } else if (message.contains("Sad")) {
-	                return "SAD";
-	            } else {
-	                return "HAPPY";
-	            }
-	        } catch (InvalidMoodException e) {
-	            // Handle the exception and return "HAPPY" as default value
-	            System.out.println(e.getMessage());
-	            return "HAPPY";
-	        }
-	    }
-	}
+    public String getValue() {
+        return value;
+    }
 
+    public static MoodEnum fromValue(String value) {
+        return map.get(value.toLowerCase());
+    }
+}
 
+// Define MoodAnalyzer class
+class MoodAnalyzer {
+    public String analyzeMood(String mood) throws MoodAnalysisException {
+        // Check if mood is null or empty
+        if (mood == null || mood.trim().isEmpty()) {
+            throw new MoodAnalysisException("Invalid Mood: Mood cannot be empty or null.");
+        }
+
+        // Convert mood to lowercase for case-insensitive comparison
+        mood = mood.toLowerCase();
+
+        // Check if mood is valid using Enum
+        MoodEnum moodEnum = MoodEnum.fromValue(mood);
+        if (moodEnum == null) {
+            throw new MoodAnalysisException("Invalid Mood: Mood not recognized.");
+        }
+
+        // Return the analyzed mood
+        return mood;
+    }
+}
 	
-
-
